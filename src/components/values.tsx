3b14e99388678d87cb4c0189adb38ffe65c168ae
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-10-27 22:55:27 +0200

    Values section completion

--- src/components/values.tsx
+++ src/components/values.tsx
@@ -19,7 +19,42 @@ export default function Values() {
                   <br /> selection of equipment and services helps you
                   <br /> express your creativity and style.
                 </p>
-                <button className="text-uppercase">Shop Now</button>
+                <button className="text-uppercase mb-5">Shop Now</button>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="values-wrap d-flex justify-content-center position-relative">
+                <img src="../../assets/values_center.png" alt="" />
+                <div className="values-small-img left-top position-absolute">
+                  <img
+                    className="w-100 h-100"
+                    src="../../assets/values_left_top.png"
+                    alt=""
+                  />
+                </div>
+                <div className="values-small-img right-top position-absolute">
+                  <img
+                    className="w-100 h-100"
+                    src="../../assets/values_right_top.png"
+                    alt=""
+                  />
+                </div>
+                <div className="values-small-img left-bot position-absolute">
+                  <img
+                    className="w-100 h-100"
+                    src="../../assets/values_left_bot.png"
+                    alt=""
+                  />
+                </div>
+                <div className="values-small-img right-bot position-absolute">
+                  <img
+                    className="w-100 h-100"
+                    src="../../assets/values_right_bot.png"
+                    alt=""
+                  />
+                </div>
               </div>
             </div>
           </div>
