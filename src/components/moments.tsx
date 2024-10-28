cf1ca48c758b1179760483f8544fe8dc29379aa1
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-10-28 17:27:02 +0200

    Moments section completion

--- src/components/moments.tsx
+++ src/components/moments.tsx
@@ -4,17 +4,26 @@ export default function Moments() {
   return (
     <>
       <section id="moments">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <div className="moments-wrap">
-                <h2>Capture Beautiful Moments</h2>
+        <div className="container-fluid">
+          <div className="row justify-content-center">
+            <div className="col-9">
+              <div className="moments-wrap position-relative d-flex flex-column justify-content-center align-items-center">
+                <h2 className="text-capitalize">Capture Beautiful Moments</h2>
                 <p className="text-center">
                   We offer professional photography services and products to
-                  help you capture life's most beautiful moments. Explore our
-                  investment strategies and savings accounts to optimize your
-                  earnings and achieve your financial goals.
+                  help you capture life's most beautiful moments.
+                  <br /> Explore our investment strategies and savings accounts
+                  to optimize your earnings and achieve your financial
+                  <br /> goals.
                 </p>
+                <button className="text-uppercase">Discover More</button>
+                <div className="position-absolute moments-img">
+                  <img
+                    src="../../assets/moments_img.png"
+                    alt=""
+                    className="w-100 h-100"
+                  />
+                </div>
               </div>
             </div>
           </div>
