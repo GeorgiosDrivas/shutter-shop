ddf73d5d8dedbb43fd6d1fa9bb1cca6a14b9222d
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-11-11 17:57:09 +0200

    Q&A accordion functionality

--- src/components/services/serviceSingleAccordion.tsx
+++ src/components/services/serviceSingleAccordion.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function SingleAccordion({
   title,
   content,
@@ -5,10 +7,20 @@ export default function SingleAccordion({
   title: string;
   content: string;
 }) {
+  const [clicked, setClicked] = useState(false);
+
   return (
-    <div className="singleAccordion">
-      <p className="mb-4">{title}</p>
-      <p>{content}</p>
+    <div className="singleAccordion-wrap">
+      <div
+        className="singleAccordion d-flex justify-content-between align-items-center"
+        onClick={() => setClicked((prv) => !prv)}
+      >
+        <p className="m-0">{title}</p>
+        <div></div>
+      </div>
+      <div className={`singleAccordion-content ${clicked ? "clicked" : ""}`}>
+        <p className="mt-4 mb-0">{content}</p>
+      </div>
     </div>
   );
 }
