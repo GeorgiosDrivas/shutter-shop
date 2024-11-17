ccc0190d84bd5089d00a03ff8b7a746002c3933f
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-11-17 20:57:16 +0200

    First article layout and styling

--- src/components/blog/blog.tsx
+++ src/components/blog/blog.tsx
@@ -18,6 +18,33 @@ export default function Blog() {
               </p>
             </div>
           </div>
+          <div className="row">
+            <div className="col-4">
+              <div className="article-wrap">
+                <div className="article-img">
+                  <img
+                    className="w-100 h-100"
+                    src="/assets/article1.png"
+                    alt="Article 1 Image"
+                  />
+                </div>
+                <div className="article-info">
+                  <div className="article-details d-flex justify-content-between align-items-center mb-3">
+                    <a>Updates</a>
+                    <p>1/16/25</p>
+                  </div>
+                  <div className="article-content">
+                    <h2>Ab Molestiae Voluptas</h2>
+                    <p>
+                      Earum recusandae quis sed. Dolor asperiores atque
+                      consequatur. Sa
+                    </p>
+                  </div>
+                  <button className="p-0 mt-4">Read Post</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
